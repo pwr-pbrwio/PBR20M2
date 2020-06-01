ce8e0984696e4953957eb22c5b4709ab0301fe2a
Michal <233146@student.pwr.edu.pl>
2020-06-01 14:31:22 +0200

    Finished preprocessing for all projects, computed clean data for all projects

--- python_scripts/gatherMutations.py
+++ python_scripts/gatherMutations.py
@@ -11,12 +11,12 @@ file = open('mutationScoresGathered.csv', 'w', newline='')
 writer = csv.writer(file)
 
 
-writer.writerow(['Project', 'PackagePath', 'MutationScore'])
+writer.writerow(['Project', 'Class', 'MutationScore'])
 for project in os.listdir(MUTATION_RESULTS):
     for test in os.listdir(os.path.join(MUTATION_RESULTS, project)):
         path = '{}/'.format(MUTATION_RESULTS) + project + '/' + test + '/**/index.html'
         mutation_files = glob.glob(path, recursive=False)
         for mutation_file in mutation_files:
-            writer.writerow([project, test, PitestHTMLParser(mutation_file).get_mutation_coverage()])
+            writer.writerow([project, test.split(".")[-1], PitestHTMLParser(mutation_file).get_mutation_coverage()])
 
 file.close()
--- r_scripts/preprocessing.R
+++ r_scripts/preprocessing.R
@@ -1,73 +1,84 @@
 library(here)
-rawData <- read.csv(here("javametrics_outputs", "output_jsoup.csv"))
+library(dplyr)
 mutationScores <- read.csv(here("python_scripts", "mutationScoresGathered.csv"))
 projects <- readLines(here("projects.csv"))
+totalData <- data.frame()
 
-#rawDataFiles <- c()
-#for (i in list.files(path=here("javametrics_outputs"), pattern=".csv")) {
-#  rawDataFiles <- c(rawDataFiles, here("javametrics_outputs", i))
-#} <-- finish preprocessing for all files
-
-# Calculate McCabe min, max, mean
-library(dplyr)
-classes <- unique(rawData[3])
-classes_nonEmpty <- classes [!apply(is.na(classes) | classes == "", 1, all),]
-classes_t <- t(classes_nonEmpty)
-
-rawData <- transform(rawData, MAX_CYCLO=paste(NA, sep=""))
-rawData <- transform(rawData, MIN_CYCLO=paste(NA, sep=""))
-rawData <- transform(rawData, MEAN_CYCLO=paste(NA, sep=""))
-
-rawData$MAX_CYCLO <- as.numeric(rawData$MAX_CYCLO)
-rawData$MIN_CYCLO <- as.numeric(rawData$MIN_CYCLO)
-rawData$MEAN_CYCLO <- as.numeric(rawData$MEAN_CYCLO)
-for(i in classes_t) {
-  single_class_col = filter(rawData, Class == i)
-  cyclo_column <- single_class_col["CYCLO"]
-  cyclo_column_non_empty <- cyclo_column[!apply(is.na(cyclo_column) | cyclo_column == "", 1, all),]
-  
-  # Compute MAX_CYCLO
-  rawData[rawData$MethodSignature == "" & rawData$Class == i, "MAX_CYCLO"] <- max(cyclo_column_non_empty)
-  
-  # Compute MIN_CYCLO
-  rawData[rawData$MethodSignature == "" & rawData$Class == i, "MIN_CYCLO"] <- min(cyclo_column_non_empty)
-
-  # Compute MEAN_CYCLO
-  rawData[rawData$MethodSignature == "" & rawData$Class == i, "MEAN_CYCLO"] <- mean(cyclo_column_non_empty)
-}
-rawData[rawData == "-Inf"] <-NA
-rawData[rawData == "Inf"] <-NA
-rawData[rawData == NaN] <-NA
-
-
-classMetrics <- rawData[(rawData$MethodSignature==""),] # select data for classes only
-classMetrics <- classMetrics[!sapply(classMetrics, function(x) all(is.na(x)))] # drop all rows without any values (columns with method metrics)
-classMetrics$Project <- as.character(classMetrics$Project) # cast Project column to character
-
-# change project absolute path to project name
-for (project in projects) {
-  classMetrics$Project[grepl(project, classMetrics$Project)] <- project
+rawDataFiles <- c()
+for (i in list.files(path=here("javametrics_outputs"), pattern=".csv")) {
+  rawDataFiles <- c(rawDataFiles, here("javametrics_outputs", i))
 }
 
-# add column with full package name, needed when merging
-classMetrics <- transform(classMetrics, PackagePath=paste(Package, Class, sep="."))
-classMetrics <- classMetrics[ , -which(names(classMetrics) %in% c("Project", "Package", 
-                                                                        "Class", "MethodSignature",
-                                                                        "OuterClass", "AccessModifier",
-                                                                        "IsStatic", "IsFinal"))]
 
-# Combine metrics for production class and test class into one row
-testMetric <- subset(classMetrics, grepl("Test", classMetrics$PackagePath))
-prodMetric <- subset(classMetrics, !grepl("Test", classMetrics$PackagePath))
-prodMetric <- transform(prodMetric, PackagePath=paste(PackagePath, "Test", sep=""))
-classMetrics <- merge(prodMetric, testMetric, by = "PackagePath")
+# Merge columns project and class from 'mutationScores', to then merge with metrics
+mutationScores <- transform(mutationScores, ProjectClass=paste(Project, Class, sep = "."))
+mutationScores <- mutationScores[ , -which(names(mutationScores) %in% c("Project", "Class"))]
 
-# Merge mutation scores with metrics
-finalDataFrame <- merge(classMetrics, mutationScores, by = "PackagePath")
-
-# Divide entries into classes
-finalDataFrame$MutationScore[finalDataFrame$MutationScore > 0.5] <- 1
-finalDataFrame$MutationScore[finalDataFrame$MutationScore <= 0.5] <- 0
-
-# Drop column PackagePath and Project to have usable data in classification
-finalDataFrame <- finalDataFrame[ , -which(names(finalDataFrame) %in% c("PackagePath", "Project"))]
+for (file in rawDataFiles) {
+  print(file)
+  rawData <- read.csv(file)
+  # Calculate McCabe min, max, mean
+  classes <- unique(rawData[3])
+  classes_nonEmpty <- classes [!apply(is.na(classes) | classes == "", 1, all),]
+  classes_t <- t(classes_nonEmpty)
+  
+  rawData <- transform(rawData, MAX_CYCLO=paste(NA, sep=""))
+  rawData <- transform(rawData, MIN_CYCLO=paste(NA, sep=""))
+  rawData <- transform(rawData, MEAN_CYCLO=paste(NA, sep=""))
+  
+  rawData$MAX_CYCLO <- as.numeric(rawData$MAX_CYCLO)
+  rawData$MIN_CYCLO <- as.numeric(rawData$MIN_CYCLO)
+  rawData$MEAN_CYCLO <- as.numeric(rawData$MEAN_CYCLO)
+  for(i in classes_t) {
+    single_class_col = filter(rawData, Class == i)
+    cyclo_column <- single_class_col["CYCLO"]
+    cyclo_column_non_empty <- cyclo_column[!apply(is.na(cyclo_column) | cyclo_column == "", 1, all),]
+    
+    # Compute MAX_CYCLO
+    rawData[rawData$MethodSignature == "" & rawData$Class == i, "MAX_CYCLO"] <- max(cyclo_column_non_empty)
+    
+    # Compute MIN_CYCLO
+    rawData[rawData$MethodSignature == "" & rawData$Class == i, "MIN_CYCLO"] <- min(cyclo_column_non_empty)
+  
+    # Compute MEAN_CYCLO
+    rawData[rawData$MethodSignature == "" & rawData$Class == i, "MEAN_CYCLO"] <- mean(cyclo_column_non_empty)
+  }
+  rawData[rawData == "-Inf"] <-NA
+  rawData[rawData == "Inf"] <-NA
+  rawData[rawData == NaN] <-NA
+  
+  
+  classMetrics <- rawData[(rawData$MethodSignature==""),] # select data for classes only
+  classMetrics <- classMetrics[!sapply(classMetrics, function(x) all(is.na(x)))] # drop all rows without any values (columns with method metrics)
+  classMetrics$Project <- as.character(classMetrics$Project) # cast Project column to character
+  
+  # change project absolute path to project name
+  for (project in projects) {
+    classMetrics$Project[grepl(project, classMetrics$Project)] <- project
+  }
+  
+  # add column with full package name, needed when merging
+  classMetrics <- transform(classMetrics, ProjectClass=paste(Project, Class, sep="."))
+  classMetrics <- classMetrics[ , -which(names(classMetrics) %in% c("Project", "Package", 
+                                                                          "Class", "MethodSignature",
+                                                                          "OuterClass", "AccessModifier",
+                                                                          "IsStatic", "IsFinal"))]
+  
+  # Combine metrics for production class and test class into one row
+  testMetric <- subset(classMetrics, grepl("Test", classMetrics$ProjectClass))
+  prodMetric <- subset(classMetrics, !grepl("Test", classMetrics$ProjectClass))
+  prodMetric <- transform(prodMetric, ProjectClass=paste(ProjectClass, "Test", sep=""))
+  classMetrics <- merge(prodMetric, testMetric, by = "ProjectClass")
+  
+  # Merge mutation scores with metrics
+  finalDataFrame <- merge(classMetrics, mutationScores, by = "ProjectClass")
+  
+  # Divide entries into classes
+  finalDataFrame$MutationScore[finalDataFrame$MutationScore > 0.5] <- 1
+  finalDataFrame$MutationScore[finalDataFrame$MutationScore <= 0.5] <- 0
+  
+  # Drop column ProjectClass and Project to have usable data in classification
+  finalDataFrame <- finalDataFrame[ , -which(names(finalDataFrame) %in% c("ProjectClass", "Project"))]
+  totalData <- rbind(totalData, finalDataFrame)
+}
+write.csv(totalData, "cleanData.csv", row.names = FALSE, append = TRUE)
