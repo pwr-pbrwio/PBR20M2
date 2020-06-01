b37007486c3f8fe1f390ec570553d0b82fd65228
Michal <233146@student.pwr.edu.pl>
2020-06-01 17:58:35 +0200

    Modified basePipeline to use the preprocessing step

--- r_scripts/basePipeline.R
+++ r_scripts/basePipeline.R
@@ -1,31 +1,19 @@
-data <- read.csv("/home/mich/Desktop/outputs/output_jsoup.csv")
-mutationScores <- read.csv("/home/mich/Desktop/PBR20M2/python_scripts/mutationScoresGathered.csv")
-projects <- readLines("/home/mich/Desktop/PBR20M2/projects.csv")
-
-classMetric <- data[(data$MethodSignature==""),]
-classMetric <- classMetric[!sapply(classMetric, function(x) all(is.na(x)))]
-classMetric$Project <- as.character(classMetric$Project)
-
-for (project in projects) {
-  classMetric$Project[grepl(project, classMetric$Project)] <- project
-}
-
-classMetric <- transform(classMetric, PackagePath=paste(Package, Class, sep="."))
-
-outputDf <- merge(classMetric, mutationScores, by = "PackagePath")
-
-outputDf$MutationScore[outputDf$MutationScore > 0.5] <- 1
-outputDf$MutationScore[outputDf$MutationScore <= 0.5] <- 0
-
-outputDf <- outputDf[ , -which(names(outputDf) %in% c("PackagePath", "Project.x", "Package", 
-                                               "Class", "MethodSignature",
-                                               "OuterClass", "AccessModifier",
-                                               "Project.y"))]
-
 library("randomForest")
+
+
+cleanData <- read.csv(here("cleanData.csv"))
+row.has.na <- apply(cleanData, 1, function(x){any(is.na(x))})
+cleanData <- cleanData[!row.has.na, ]
+cleanData$MutationScore <- as.factor(cleanData$MutationScore)
+
 set.seed(1337)
-ind = sample(2, nrow(outputDf), replace = TRUE, prob=c(0.7, 0.3))
-trainData = outputDf[ind==1,]
-testData = outputDf[ind==2,]
-rF = randomForest(MutationScore~., data=trainData, ntree=100, proximity=T)
-plot(rF)
+ind = sample(2, nrow(cleanData), replace = TRUE, prob=c(0.7, 0.3))
+trainData = cleanData[ind==1,]
+testData = cleanData[ind==2,]
+
+#print(sum(row.has.na))
+rF = randomForest(MutationScore~., data=trainData, ntree=500, proximity=T)
+print(rF)
+
+out <- predict(rF, testData, predict.all = TRUE)
+#print(out)
