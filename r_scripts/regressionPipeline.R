53858a57d104a14ad72937ab5d1926232a10809b
BartoszBoczar <bboczara2010@op.pl>
2020-06-25 21:09:56 +0200

    Added regression preprocessing and pipeline

---
+++ r_scripts/regressionPipeline.R
@@ -0,0 +1,96 @@
+library(pacman)
+p_load(paradox)
+p_load(mlr3)
+p_load(mlr3learners)
+p_load(mlr3tuning)
+p_load(here)
+p_load(mlr3filters)
+if(!require(mlr3learners.randomforest)){
+  install.packages("mlr3learners.randomforest", repos = "https://mlr3learners.github.io/mlr3learners.drat")
+}
+library(mlr3learners.randomforest)
+
+#learnerTypeList = c("regr.randomForest", "regr.kknn", "regr.svm")
+learnerTypeList = c("regr.svm")
+
+
+cleanData <- read.csv(here("cleanData.csv"))
+row.has.na <- apply(cleanData, 1, function(x){any(is.na(x))})
+cleanData <- cleanData[!row.has.na, ]
+
+cleanData$ALU.x <- as.integer(as.logical(cleanData$ALU.x))
+cleanData$ALU.y <- as.integer(as.logical(cleanData$ALU.y))
+
+cleanData <- as.data.frame(sapply(cleanData, as.numeric))
+#``cleanData$MutationScore <- as.factor(cleanData$MutationScore)
+
+
+set.seed(1337)
+
+for(learnerType in learnerTypeList) {
+  learner <- lrn(learnerType)
+  resampling <- rsmp("holdout")
+  measures <- msrs(c("regr.rmse", "regr.sse"))
+  tune_ps <- NA
+  if(learnerType == "regr.randomForest") {
+    rfLearner <- learner
+    learner$param_set$values = list(importance = "mse")
+    tune_ps <- ParamSet$new(list(
+      ParamInt$new(id = "ntree", lower = 300, upper = 800),
+      ParamInt$new(id = "mtry", lower = 10, upper = 40),
+      ParamInt$new(id = "nodesize", lower = 5, upper = 20)
+    ))
+  } else if(learnerType == "regr.kknn") {
+    knnLearner <- learner
+    tune_ps <- ParamSet$new(list(
+      ParamInt$new(id = "k", lower = 1, upper = 20),
+      ParamDbl$new(id = "distance", lower = .001, upper = 2)
+    ))
+  } else if(learnerType == "regr.svm") {
+    learner$param_set$values = list(type = "eps-regression", kernel = "radial")
+    svmLearner <- learner
+    tune_ps <- ParamSet$new(list(
+      ParamDbl$new(id = "cachesize", lower = 20, upper = 150),
+      ParamDbl$new(id = "cost", lower = 30, upper = 80),
+      ParamDbl$new(id = "gamma", lower = 0.00001, upper = 0.001)
+    ))
+  }
+  
+  terminator <- term("evals", n_evals = 2)
+  tuner <- tnr("grid_search")
+  
+  at = AutoTuner$new(
+    learner = learner,
+    resampling = resampling,
+    measures = measures,
+    tune_ps = tune_ps,
+    terminator = terminator,
+    tuner = tuner
+  )
+  print(at)
+  at$store_tuning_instance = TRUE
+  
+  resampling_outer = rsmp("cv", folds = 10)
+  testTask = TaskRegr$new(id = "tests", backend = cleanData, target = "MutationScore")
+  rr <- resample(task = testTask, learner = at, resampling = resampling_outer, store_models = TRUE)
+  
+  print(rr$aggregate())
+  
+  # Find best of created models
+  best_rmse <- Inf
+  for (i in 1:10) {
+    if (rr$data$learner[[i]]$tuning_result$perf["regr.rmse"] < best_rmse) {
+      best_regr <- rr$data$learner[[i]]
+      best_rmse <- rr$data$learner[[i]]$tuning_result$perf["regr.rmse"]
+    }
+  }
+  write.csv(best_regr$tuning_result$perf, paste(here("performance"), "/", learnerType, ".csv", sep = ""))
+  
+  save(best_regr, file=paste("saved_models/", learnerType, ".RData", sep = ""))
+  
+  if(learnerType == "regr.randomForest") {
+    print(best_regr$model$learner$importance())
+  }
+}
+
+
---
+++ r_scripts/regressionPreprocessing.R
@@ -0,0 +1,83 @@
+library(pacman)
+p_load(here)
+p_load(dplyr)
+
+mutationScores <- read.csv(here("mutationScoresGathered.csv"))
+projects <- read.csv(here("projects.csv"))
+totalData <- data.frame()
+
+rawDataFiles <- c()
+for (i in list.files(path=here("javametrics_outputs"), pattern=".csv")) {
+  rawDataFiles <- c(rawDataFiles, here("javametrics_outputs", i))
+}
+
+# Merge columns project and class from 'mutationScores', to then merge with metrics
+mutationScores <- transform(mutationScores, ProjectClass=paste(Project, Class, sep = "."))
+mutationScores <- mutationScores[ , -which(names(mutationScores) %in% c("Project", "Class"))]
+
+for (file in rawDataFiles) {
+  print(file)
+  rawData <- read.csv(file)
+  # Calculate McCabe min, max, mean
+  classes <- unique(rawData[3])
+  classes_nonEmpty <- classes [!apply(is.na(classes) | classes == "", 1, all),]
+  classes_t <- t(classes_nonEmpty)
+  
+  rawData <- transform(rawData, MAX_CYCLO=paste(NA, sep=""))
+  rawData <- transform(rawData, MIN_CYCLO=paste(NA, sep=""))
+  rawData <- transform(rawData, MEAN_CYCLO=paste(NA, sep=""))
+  
+  rawData$MAX_CYCLO <- as.numeric(rawData$MAX_CYCLO)
+  rawData$MIN_CYCLO <- as.numeric(rawData$MIN_CYCLO)
+  rawData$MEAN_CYCLO <- as.numeric(rawData$MEAN_CYCLO)
+  for(i in classes_t) {
+    single_class_col = filter(rawData, Class == i)
+    cyclo_column <- single_class_col["CYCLO"]
+    cyclo_column_non_empty <- cyclo_column[!apply(is.na(cyclo_column) | cyclo_column == "", 1, all),]
+    
+    # Compute MAX_CYCLO
+    rawData[rawData$MethodSignature == "" & rawData$Class == i, "MAX_CYCLO"] <- max(cyclo_column_non_empty)
+    
+    # Compute MIN_CYCLO
+    rawData[rawData$MethodSignature == "" & rawData$Class == i, "MIN_CYCLO"] <- min(cyclo_column_non_empty)
+    
+    # Compute MEAN_CYCLO
+    rawData[rawData$MethodSignature == "" & rawData$Class == i, "MEAN_CYCLO"] <- mean(cyclo_column_non_empty)
+  }
+  rawData[rawData == "-Inf"] <-NA
+  rawData[rawData == "Inf"] <-NA
+  rawData[rawData == NaN] <-NA
+  
+  
+  classMetrics <- rawData[(rawData$MethodSignature==""),] # select data for classes only
+  classMetrics <- classMetrics[!sapply(classMetrics, function(x) all(is.na(x)))] # drop all rows without any values (columns with method metrics)
+  classMetrics$Project <- as.character(classMetrics$Project) # cast Project column to character
+  
+  # change project absolute path to project name
+  for (project in projects$project) {
+    classMetrics$Project[grepl(project, classMetrics$Project)] <- project
+  }
+  
+  # add column with full package name, needed when merging
+  classMetrics <- transform(classMetrics, ProjectClass=paste(Project, Class, sep="."))
+  classMetrics <- classMetrics[ , -which(names(classMetrics) %in% c("Project", "Package", 
+                                                                    "Class", "MethodSignature",
+                                                                    "OuterClass", "AccessModifier",
+                                                                    "IsStatic", "IsFinal"))]
+  
+  # Combine metrics for production class and test class into one row
+  testMetric <- subset(classMetrics, grepl("Test", classMetrics$ProjectClass))
+  prodMetric <- subset(classMetrics, !grepl("Test", classMetrics$ProjectClass))
+  prodMetric <- transform(prodMetric, ProjectClass=paste(ProjectClass, "Test", sep=""))
+  classMetrics <- merge(prodMetric, testMetric, by = "ProjectClass")
+  
+  # Merge mutation scores with metrics
+  finalDataFrame <- merge(classMetrics, mutationScores, by = "ProjectClass")
+  
+  # Drop column ProjectClass and Project to have usable data in classification
+  finalDataFrame <- finalDataFrame[ , -which(names(finalDataFrame) %in% c("ProjectClass", "Project"))]
+  totalData <- rbind(totalData, finalDataFrame)
+}
+
+write.csv(totalData, "cleanData.csv", row.names = FALSE, append = TRUE)
+
